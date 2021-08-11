e6aabbde1ce2591561cb2b574ed2a3180b57ab5b
Ivan Notaros <nothke@gmail.com>
2021-08-11 18:27:57 +0200

    Added cube throwing for collider testing

---
+++ Assets/Scripts/ThrowCubes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowCubes : MonoBehaviour
+{
+    public GameObject prefab;
+    public float speed = 30;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var go = Instantiate(prefab, transform.position + transform.forward, Random.rotation);
+            go.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        }
+    }
+}
